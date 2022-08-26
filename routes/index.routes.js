54a9737dac2d02c521528711142d66a75dae4354
Adriana Leitao <adrianaleitao5_8@hotmail.com>
2022-08-26 15:35:02 +0100

    cloudinary almost done

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -6,6 +6,16 @@ router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
 
+router.post("/upload", fileUploader.single("imageUrl"), (req, res, next) => {
+ 
+  if (!req.file) {
+    next(new Error("No file uploaded!"));
+    return;
+  }
+
+  res.json({ fileUrl: req.file.path });
+});
+
 router.use("/auth", authRoutes);
 
 module.exports = router;
